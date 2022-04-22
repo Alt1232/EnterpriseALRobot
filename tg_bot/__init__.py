9121e82889cdce1c21de938e0e5d71fc8fbe424a
Alt1232 <95954988+Alt1232@users.noreply.github.com>
2022-04-22 20:09:04 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -43,7 +43,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 7:
     quit(1)
 
 parser = ConfigParser()
-parser.read("config.ini")
+parser.read("config.py")
 kigconfig = parser["kigconfig"]
 
 class KigyoINIT:
